2a4ffab5aedf834437ec8c06e34198606dcd5601
dev-rahim <rahimuddin5915@gmail.com>
2023-03-12 12:51:53 +0600

    fetch data from our server done

--- src/Components/Shared/LeftSIdeNav/LeftSIdeNav.js
+++ src/Components/Shared/LeftSIdeNav/LeftSIdeNav.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftSIdeNav = () => {
+    const [category, setCategory] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/category')
+            .then(res => res.json())
+            .then(data => {
+                setCategory(data)
+                console.log(data);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
     return (
         <div>
-            <h2>Left Side Nav</h2>
+            <h2>{category[0]?.name}</h2>
         </div>
     );
 };
