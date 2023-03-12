bf0513524007b1495f7c57084122b908cfa6461a
dev-rahim <rahimuddin5915@gmail.com>
2023-03-12 11:35:35 +0600

    initialize primary setup of and setup the react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-
+import Home from './Components/Home/Home';
+import Main from './Components/Layout/Main/Main';
+const router = createBrowserRouter([
+  {
+    path: '/', element: <Main />, children: [
+      { path: '/', element: <Home /> }
+    ]
+  },
+])
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB7vm0D614cOVjmiilQZmAEsbttiGwF_BU",
+    authDomain: "daylight-news-4060e.firebaseapp.com",
+    projectId: "daylight-news-4060e",
+    storageBucket: "daylight-news-4060e.appspot.com",
+    messagingSenderId: "273420123328",
+    appId: "1:273420123328:web:49174493cb0c7fe5784125"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
